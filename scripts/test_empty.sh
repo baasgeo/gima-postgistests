0aa90e0c919a3cdeefd5840c70f320cdd93356dd
Bart Baas <bartbaas@gmail.com>
2012-05-24 13:18:11 +0200

    Eliminate python startup time for empty operation

--- scripts/test_empty.sh
+++ scripts/test_empty.sh
@@ -2,4 +2,10 @@
 DBNAME=$1
 START=$(python -c'import time; print repr(time.time())')
 
+END1=$(python -c'import time; print repr(time.time())')
+PTIME=$(bc -l <<< $END1-$START)
+ECHO "Python: " $(bc -l <<< $PTIME)
+
 psql -d $DBNAME -f sql/test_empty.sql
+END2=$(python -c'import time; print repr(time.time())')
+ECHO "PSQL: " $(bc -l <<< $END2-$END1-$PTIME)
---
+++ sql/test_empty.sql
@@ -0,0 +1,3 @@
+-- SQL empty call
+-- Sends an empty SQL to the server
+SELECT 'Nothing to do' AS TEST_EMPTY;
